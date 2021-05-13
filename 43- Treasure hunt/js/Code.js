24003c03805aab6571a5ad49652bfc73c13d03a8
Anshika0309 <73583661+Anshika0309@users.noreply.github.com>
2021-05-13 10:45:36 +0530

    Add files via upload

---
+++ 43- Treasure hunt/js/Code.js
@@ -0,0 +1,28 @@
+const accessCode1 = "ARRAY";
+const accessCode3 = "DRAW";
+const accessCode2 = "DATABASE";
+
+function clues() {
+    
+    fill("white")
+    textSize(15)
+    text("R A Y A R", 100,50)
+    fill("lightblue")
+    text("Hint: Contains various variables inside [] brackets. Enter in CAPS !!", 100,70)
+// add code for displaying rest of the names and the hints.
+
+fill("white")
+    textSize(15)
+    text("A R D W", 240,280)
+    fill("lightblue")
+    text("Hint: Name the function that runs multiple times in sketch!!", 240,300)
+    
+
+    fill("white")
+    textSize(15)
+    text(" A T A D B E S A", 675,130)
+    fill("lightblue")
+    text("Hint: Stores all the information!!", 675,150);
+    
+
+}
